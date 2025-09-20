dec8646002004c5554817d3ebb1580c2c6e22254
JoseArmandoSUP <124048843@upq.edu.mx>
2025-09-19 20:33:48 -0600

    Inciso c)
    Se crea un saludo personalizado como una función de flecha con 2 parametros: el nombre y la edad, se retornan siguiendo el limite de las llaves. Luego se mandan a llamar con console.log con 3 ejemplos de ejecución

---
+++ Practica1/c.js
@@ -0,0 +1,6 @@
+const saludoPersonalizado = (nombre, edad) => 
+    {return "Hola, me llamo " + nombre + " y tengo " + edad + " años."};
+
+console.log(saludoPersonalizado("Armando", 19));
+console.log(saludoPersonalizado("Isay", 37));
+console.log(saludoPersonalizado("Decuandre", 3000));
