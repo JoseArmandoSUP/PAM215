ed5fc289b096ca84408048e1464be6e607cafd1d
JoseArmandoSUP <124048843@upq.edu.mx>
2025-09-22 22:01:14 -0600

    Inciso c) completo
    Se tiene un arreglo que lista a 3 personas, con su nombre y su edad, primero se va a buscar a la persona de la lista llamada "Luis" con el método .find(), si se encuentra mostrará a Luis junto a su nombre, esto con la ayuda de una variable temporal en lo que recorre el arreglo. Después, con el método .forEach() se van a listar a todas las personas junto a sus edades. Finalmente con el método .reduce(), se van a sumar todas las edades, con la variable "suma" que va a ir acumulando las edades al inicializarla en 0, mientras que "op" va a ir recorriendo el arreglo  para acceder a la edad de cada persona y así sacar la suma

--- Practica2/c.js
+++ Practica2/c.js
@@ -4,5 +4,10 @@ const personas = [
     {nombre: "Maria", edad: 28}
 ];
 
-const encontrarLuis = personas.find(enLuis => enLuis.nombre === "Luis");
-console.log(encontrarLuis + " encontrado");
+const encontrarLuis = personas.find(op => op.nombre === "Luis");
+console.log(encontrarLuis, " encontrado :D");
+
+personas.forEach(op => {console.log(op.nombre + " | " + op.edad + " años");});
+
+const sumarEdades = personas.reduce((suma, op) => suma + op.edad, 0);
+console.log("Suma de todas las edades: " + sumarEdades);
