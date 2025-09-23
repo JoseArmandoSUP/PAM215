c01af0bf5c1f1ff7ec9de7913bebf5ba093e57ec
JoseArmandoSUP <124048843@upq.edu.mx>
2025-09-22 22:05:22 -0600

    Inciso a)
    Se tiene el objeto "persona"  con sus valores nombre, edad y dirección, dividida en ciudad y país. Se van a extraer sus valores por medio de la deconstrucción para formar un mensaje. Los valores se extraen y se asignan a valores independientes

--- Practica2/a.js
+++ Practica2/a.js
@@ -7,5 +7,5 @@ const persona = {
     }
 };
 
-const {nombre, edad, direccion: {ciudad, pais}} = persona
-console.log("Me llamo " + nombre + ", tengo " + edad + " años y vivo en " + ciudad + " " + pais);
+const {nombre, edad, direccion: {ciudad}} = persona
+console.log("Me llamo " + nombre + ", tengo " + edad + " años y vivo en " + ciudad);
