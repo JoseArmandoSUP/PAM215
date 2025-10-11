d71793ed53509befc1d0526d8ae1483354c5e15e
JoseArmandoSUP <124048843@upq.edu.mx>
2025-10-10 20:23:56 -0600

    Agregar los botones 'Agregar', 'Quitar', 'Reiniciar'
    Se agregaron 3 funciones "Buttons" para agregar 3 botones que van a aumentar, restar y reiniciar un contador que se le enlaza mediante el Hook useState, creando la variable "contador" que es el valor inicial y va a ir cambiando por medio del props "onPress" . Se va a deconstruir useState, sacando contador y setContador

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,25 @@
+//1. Imports: Zona de importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import React,{useState} from 'react';
 
+//2. Main: Zona de componentes
 export default function App() {
+  const[contador,setContador] = useState(0);
   return (
+
     <View style={styles.container}>
-      <Text>Hola mundo</Text>
+      <Text>Contador:{contador}</Text>
+      <Button title='Agregar' onPress={()=>setContador(contador+1)}> </Button>
+      <Button title='Quitar' onPress={()=>setContador(contador-1)}> </Button>
+      <Button title='Reiniciar' onPress={()=>setContador(0)}> </Button>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
+//3. Estilos: Zona de estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
