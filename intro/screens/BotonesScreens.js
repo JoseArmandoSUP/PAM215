5005e61a2460ca3939794851def8e1eaf5d1a504
JoseArmandoSUP <124048843@upq.edu.mx>
2025-10-14 20:02:33 -0600

    Pantalla de botones
    Únicamente se le agrega un texto que anuncia la practica de botones

---
+++ intro/screens/BotonesScreens.js
@@ -0,0 +1,13 @@
+import { Text, StyleSheet, View } from 'react-native'
+
+export default function BotonesScreens() {
+  
+    return (
+      <View>
+        <Text>proximamente... por The Daniels y Richi</Text>
+      </View>
+    )
+
+}
+
+const styles = StyleSheet.create({})
