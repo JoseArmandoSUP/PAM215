a20b5ab8551e91afabcbbc2854ec672e70bf87c6
JoseArmandoSUP <124048843@upq.edu.mx>
2025-10-17 23:43:47 -0600

    BottomSheet

--- intro/screens/ButtomSheet.js
+++ intro/screens/ButtomSheet.js
@@ -1,54 +1,13 @@
-import React, {useCallback, useRef, useState} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Text, StyleSheet, View, Button } from 'react-native'
-import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import { Text, StyleSheet, View } from 'react-native'
 
 export default function ButtomSheet () {
   
-  const sheetRef = useRef(null);
-  const [isOpen, setIsOpen] = useState(false);
-  const snapPoints = ["40%"];
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>ButtomSheet Demo</Text>
-
-      <Button
-        title={isOpen ? "Cerrar" : "Abrir BottomSheet"}
-        onPress={() => {
-          if(isOpen){
-            sheetRef.current?.close();
-          }else{
-            sheetRef.current?.expand();
-          }
-          setIsOpen(!isOpen);
-        }} 
-      />
-
-      <BottomSheet ref={sheetRef} index={-1} snapPoints={snapPoints}>
-        <BottomSheetView style={styles.sheetContent}>
-          <Text>Hola</Text>
-        </BottomSheetView>
-      </BottomSheet>
-    </View>
-  );
+    return (
+      <View>
+        <Text>ButtomSheet</Text>
+      </View>
+    )
   
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'yellow',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  sheetContent: {
-    flex: 1,
-    alignItems: 'row',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 10,
-  },
-});
+const styles = StyleSheet.create({})
