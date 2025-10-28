9a246edc7d6704664e68bf2f90bd19841fa83b53
JoseArmandoSUP <124048843@upq.edu.mx>
2025-10-27 20:24:02 -0600

    ScrollView : Completada

--- intro/screens/ScrollView.js
+++ intro/screens/ScrollView.js
@@ -1,13 +1,170 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, ScrollView, Button } from 'react-native'
+import React, {useState, useRef} from 'react';
 
-export default function ScrollView(){
+export default function Scrollview(){
   
-    return (
+  const scrollRef=useRef();
+  const irAlFinal=()=>{
+    scrollRef.current.scrollToEnd({animated: true});
+  }
+
+  return (
+    <ScrollView
+      ref={scrollRef}
+      style={styles.container}
+      contentContainerStyle={styles.content}
+      showsVerticalScrollIndicator={true}
+    >
+
+      <Text style={styles.titulo}>Practica: ScrollView</Text>
+      <Text style={styles.titulo2}>Ejemplo de desplazamiento vertical</Text>
+      
       <View>
-        <Text>proximamente... por el equipo de Monse</Text>
+        <Button
+          color='#35f7eaff'
+          title='Ir al final'
+          onPress={irAlFinal}
+        ></Button>
       </View>
-    )
+
+      <View style={styles.elementos}>
+        <Text style={styles.text}>Elemento 1</Text>
+      </View>
+
+      <View style={styles.elementos}>
+        <Text style={styles.text}>Elemento 2</Text>
+      </View>
+
+      <View style={styles.elementos}>
+        <Text style={styles.text}>Elemento 3</Text>
+      </View>
+
+      <View style={styles.elementos}>
+        <Text style={styles.text}>Elemento 4</Text>
+      </View>
+
+      <View style={styles.elementos}>
+        <Text style={styles.text}>Elemento 5</Text>
+      </View>
+
+      <Text style={styles.titulo2}>Ejemplo de desplazamiento horizontal</Text>      
+      
+      <ScrollView 
+        horizontal
+        nestedScrollEnabled={true}
+        style={styles.scrollhorizontal}
+        showsHorizontalScrollIndicator={true}
+      >
+        
+        <View style={styles.elementos2}>
+          <Text style={styles.text}>Cuadro 1</Text>
+        </View>
+
+        <View style={styles.elementos2}>
+          <Text style={styles.text}>Cuadro 2</Text>
+        </View>
+
+        <View style={styles.elementos2}>
+          <Text style={styles.text}>Cuadro 3</Text>
+        </View>
+
+        <View style={styles.elementos2}>
+          <Text style={styles.text}>Cuadro 4</Text>
+        </View>
+
+        <View style={styles.elementos2}>
+          <Text style={styles.text}>Cuadro 5</Text>
+        </View>
+
+        <View style={styles.elementos2}>
+          <Text style={styles.text}>Cuadro 6</Text>
+        </View>
+
+        <View style={styles.elementos2}>
+          <Text style={styles.text}>Cuadro 7</Text>
+        </View>
+
+        <View style={styles.elementos2}>
+          <Text style={styles.text}>Cuadro 8</Text>
+        </View>
+
+        <View style={styles.elementos2}>
+          <Text style={styles.text}>Cuadro 9</Text>
+        </View>
+
+        <View style={styles.elementos2}>
+          <Text style={styles.text}>Cuadro 10</Text>
+        </View>
+
+        <View style={styles.elementos2}>
+          <Text style={styles.text}>Cuadro 11</Text>
+        </View>
+
+      </ScrollView>
+
+    </ScrollView>
+  );
   
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: 'yellow',
+  },
+
+  content:{
+    padding: 20,
+    paddingBottom: 40,
+  },
+
+  titulo:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#363333ff',
+    textAlign: 'center',
+  },
+
+  titulo2:{
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginTop: 20,
+    textAlign: 'center',
+  },
+
+  elementos:{
+    width: '100%',
+    height: 100,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignContent: 'center',
+    marginVertical: 10,
+    borderRadius: 10,
+  },
+
+  elementos2:{
+    width: 120,
+    height: 120,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignContent: 'center',
+    marginRight: 10,
+    borderRadius: 10,
+  },
+
+  text:{
+    fontSize: 10, 
+    fontFamily: 'Courier',
+    color: '#020202ff',
+    fontWeight: '900',
+    textDecorationLine: 'underline',
+    textAlign: 'center',
+  },
+
+  scrollhorizontal:{
+    marginVertical: 10,
+    width: '100%',
+  },
+
+})
