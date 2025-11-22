bbea76947b86b4838f7f728a938a30de2d6ea085
JoseArmandoSUP <124048843@upq.edu.mx>
2025-11-21 23:15:31 -0600

    Bottom Tabs Navigator Completada

---
+++ tabsNavigation/screens/settings.js
@@ -0,0 +1,35 @@
+import {View, Text, StyleSheet} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+export default function Settings(){
+    return(
+        <View style={styles.container}>
+            <View style={styles.iconRow}>
+                <Ionicons name='settings-outline' size={28} color='blue'></Ionicons>
+                <Text style={styles.titulo}>Configuraciones de usuario</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+
+    iconRow:{
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+
+    titulo:{
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginLeft: 10,
+        color: 'blue',
+    },
+});
